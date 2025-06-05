2a3af5903912da840b22aad4e434455f416e36a9
unsword01 <48459203+unsword01@users.noreply.github.com>
2025-06-05 13:16:08 -0400

    Cleanup notes

--- sketch_AnyTone_PTT_v4.ino
+++ sketch_AnyTone_PTT_v4.ino
@@ -1,7 +1,8 @@
-// Translate PTT from an input pin to serial data for AnyTone Transceiver 
+// Translate PTT from an input pin to serial data for AnyTone AT-779UV Transceiver 
+// Use LED on board as a 'Tally light' for the PTT state
 
 // Based on info in https://melik.cz/posts/sstv-first-steps/
-// Serial Port config is 115200 Baud, 8 bits data frame, 1 stop bit, no flow control
+// Serial Port config is 115200 Baud, 8 bits data frame, 1 stop bit, No Parity with no flow control
 // PTT activate:   0x41 0x01 0x00 0x00 0x00 0x00 0x00 0x06
 // PTT deactivate: 0x41 0x00 0x00 0x00 0x00 0x00 0x00 0x06
 // also needs 0x60 to be sent as keepalive after PTT activated to avoid 5 sec timeout
@@ -24,7 +25,7 @@ int sleepTime = 50;              // sleep delay for loop in ms
 int keepaliveTime = 2;           // time between keepalive messages in seconds
 int counter = 1;                 // initialize the counter at one
 int countMax = (keepaliveTime * 1000) / sleepTime; // Max value for counter before keepalive is sent
-int pttPin = 7;                  // PTT line connected to digital pin 7
+int pttPin = 7;                  // PTT line uses digital pin 7
 int ledPin = 13;                 // LED conected to pin 13
 bool pttState;                   // state of PTT line
 bool lastPttState = true;        // init PTT state (true = off)
